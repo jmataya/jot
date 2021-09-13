9ed2fa8eb964b8b0e1edff6c7fed8c602fdcc3f4
Jeff Mataya <hi@jeffmataya.com>
2021-09-13 11:49:10 -0400

    Initial Commit

---
+++ jot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  jot
+//
+//  Created by Jeffrey Mataya on 9/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
